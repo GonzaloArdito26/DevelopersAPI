af2fbf80f93b49ef7c5b8f504dadf4573120dcb3
Gonzalo <gonzalo.ardito26@outlook.com>
2018-05-01 15:28:01 -0300

    Add 2nd service

---
+++ Developers.API2/Controllers/DevelopersController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Developers.Model.Models.Developers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Developers.API2.Controllers
+{
+    [Route("api/[controller]")]
+    public class DevelopersController : Controller
+    {
+        // GET api/Developers/TopLevel
+        [HttpGet]
+        [Route("TopLevel")]
+        public async Task<IEnumerable<Developer>> GetTopLevelDevelopersAsync()
+        {
+            return await Task.Run(() => new List<Developer>() );
+        }
+    }
+}
